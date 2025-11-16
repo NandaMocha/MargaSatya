ef9ae7a64c8568cdc414620e13286c311dbce562
Nanda Mochammad <nanda.mocha@gmail.com>
2025-11-16 22:04:33 +0700

    chore: init project

---
+++ MargaSatya/MargaSatya/MargaSatyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MargaSatyaApp.swift
+//  MargaSatya
+//
+//  Created by Nanda Mochammad on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MargaSatyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
